2ea27d70448fb6623e4b725b852b2a1e3ce09e7c
tk07Sky <recode.seadex777@gmail.com>
2020-03-14 01:13:38 +0900

    feat: sanitize-htmlのオプション追加

--- src/components/utils/MarkdownConverter.vue
+++ src/components/utils/MarkdownConverter.vue
@@ -15,7 +15,46 @@ export default {
   },
   computed: {
     convertedTemplate() {
-      return sanitizeHtml(marked(this.mdText));
+      return sanitizeHtml(marked(this.mdText), {
+        allowedTags: [
+          'h1',
+          'h2',
+          'h3',
+          'h4',
+          'h5',
+          'h6',
+          'blockquote',
+          'p',
+          'a',
+          'ul',
+          'ol',
+          'nl',
+          'li',
+          'b',
+          'i',
+          'strong',
+          'em',
+          'strike',
+          'code',
+          'hr',
+          'br',
+          'div',
+          'table',
+          'thead',
+          'caption',
+          'tbody',
+          'tr',
+          'th',
+          'td',
+          'pre',
+          'iframe',
+        ],
+        allowedAttributes: {
+          '*': ['class', 'style', 'width', 'height'],
+          a: ['href', 'name', 'target', 'class', 'style', 'width', 'height'],
+          img: ['src', 'class', 'style', 'width', 'height'],
+        },
+      });
     },
   },
 };
